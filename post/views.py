25972eb06098e33464aed99d294b7894a01c093e
Amber Volkmann <52728276+AmberVolkmann@users.noreply.github.com>
2020-05-27 15:12:41 -0500

    created index to display items on post page

--- post/views.py
+++ post/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 # def is how we create a function/method in python
 def index(request):
-    return HttpResponse('Hello from post!')
+    # return HttpResponse('Hello from post!')
+
+    return render(request, 'posts/index.html')
